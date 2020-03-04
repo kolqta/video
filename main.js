40fc4c82dc8ae5f9a0380a4eda0308772fe23bf1
kolqta <kolqta@abv.bg>
2020-03-04 16:25:43 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+// The former are used to make JSHint more strict while the latter are used to suppress some warnings.
+function main(a, b) {
+	return a == null;
+}
+
+/*
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+});
+
+app.listen(8000, () => {
+  console.log('Example app listening on port 8000!')
+});
+package.json
+  "dependencies": {
+    "bootstrap": "^3.4.1",
+    "commonjs": "0.0.1",
+    "dependencies": "0.0.1",
+    "express": "^4.17.1",
+    "http-streaming": "^1.0.4",
+    "record": "^0.2.0",
+    "requirejs": "^2.3.6",
+    "videojs": "^1.0.0",
+    "videojs-record": "^3.11.0",
+    "wavesurfer": "^1.3.4",
+    "webm-wasm": "^0.4.1"
+  },
+*/
